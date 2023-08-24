19e45880d7c84c785a04bffcab1e39ce448357bc
2112tamanna <138111926+2112tamanna@users.noreply.github.com>
2023-08-25 02:28:13 +0530

    Update app.py

--- duplicate_question_pair/app.py
+++ duplicate_question_pair/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import helper
 import pickle
 
-model = pickle.load(open('model.pkl','rb'))
+model = pickle.load(open(os.path.join('duplicate_question_pair','model.pkl'),'rb'))
 
 st.header('Duplicate Question Pairs')
 
